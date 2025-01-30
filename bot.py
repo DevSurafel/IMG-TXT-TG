ed512efbfc04f0eaef218225daab248cd7c3f657
Surafel Diriba <119774330+DevSurafel@users.noreply.github.com>
2025-01-31 01:12:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,7 +46,7 @@ async def send_message_with_image():
 async def main():
     while True:
         await send_message_with_image()
-        await asyncio.sleep(600)  # Wait for 10 minutes
+        await asyncio.sleep(300)  # Wait for 10 minutes
 
 if __name__ == "__main__":
     asyncio.run(main())
