4d0ddfe0a84fc3aa805217a6e091b89fd034f86d
Surafel Diriba <119774330+DevSurafel@users.noreply.github.com>
2025-01-31 12:26:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,16 @@
 import os
 import asyncio
 from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.constants import ParseMode  # Updated import for newer versions
+from telegram.constants import ParseMode  
 from dotenv import load_dotenv
 
-# Load environment variables from the .env file
 load_dotenv()
 
-# Fetch the bot token and chat ID from environment variables
 API_TOKEN = os.getenv('BOT_TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
 
-# Replace with the file path of the image
 IMAGE_PATH = 'tg.jpg'
 
-# Replace with the message text
 MESSAGE_TEXT = (
     "Akkamiin Add gochuu dandeenya? \n\n"
     "👤 Add Members kan jedhu tuquun yoo xiqqaate nama 200 add gochuun qarshii 10,000 badhaafamaa. \n\n"
@@ -24,14 +20,11 @@ MESSAGE_TEXT = (
 
 async def send_message_with_image():
     try:
-        # Initialize the bot
         bot = Bot(token=API_TOKEN)
 
-        # Create an inline keyboard with the Contact button
         contact_button = InlineKeyboardButton("Add Bank Account ➕", url="https://t.me/Digital_Birr_Bot?start=ar6222905852")
         keyboard = InlineKeyboardMarkup([[contact_button]])
 
-        # Send the image with the message text and the inline button
         await bot.send_photo(
             chat_id=CHAT_ID,
             photo=open(IMAGE_PATH, 'rb'),
@@ -46,7 +39,7 @@ async def send_message_with_image():
 async def main():
     while True:
         await send_message_with_image()
-        await asyncio.sleep(300)  # Wait for 10 minutes
+        await asyncio.sleep(300) 
 
 if __name__ == "__main__":
     asyncio.run(main())
